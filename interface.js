317bf6bb1b5f7b0c2432500aaa89f7173bc7081d
fwill22 <79596926+fwill22@users.noreply.github.com>
2021-05-06 18:37:40 +0100

    Added interface

---
+++ interface.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMContentLoaded", function() {
+    updateTemperature = function() {
+    document.querySelector('#getCurrentTemperature').innerText = thermostat.temperature
+  }
+
+  const thermostat = new Thermostat();
+  updateTemperature();
+
+
+  document.querySelector('#increaseTemp').addEventListener('click', function() {
+    thermostat.increaseTemp()
+    updateTemperature();
+  });
+
+  document.querySelector('#decreaseTemp').addEventListener('click', () => {
+    thermostat.decreaseTemp()
+    updateTemperature();
+  });
+
+  document.querySelector('#reset').addEventListener('click', () => {
+    thermostat.reset()
+    updateTemperature();
+  });
+
+  document.querySelector('#switchOnPowerSave').addEventListener('click', () => {
+    thermostat.switchOnPowerSave()
+    document.querySelector('#power-saving-status').innerText = 'on';
+    updateTemperature();
+  });
+
+  document.querySelector('#switchOffPowerSave').addEventListener('click', () => {
+    thermostat.switchOffPowerSave()
+    document.querySelector('#power-saving-status').innerText = 'off';
+    updateTemperature();
+  });
+
+});
